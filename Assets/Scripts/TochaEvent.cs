eb76c8cb958e9a8fe998b4ec290a9be268851969
Diogo Duarte <diogord@al.insper.edu.br>
2023-10-19 16:58:39 -0300

    Tocha funcionando e quando o projetil é atingido ele acende

---
+++ Assets/Scripts/TochaEvent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TochaEvent : MonoBehaviour
+{
+    public GameObject lightPrefab; // Reference to the prefab of the light you want to spawn.
+
+    // This method is called when another object enters the trigger collider.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the entering object has a specific tag or component.
+        if (other.CompareTag("Projetil"))
+        {
+            // Spawn a light when the player enters the circle.
+            SpawnLight();
+        }
+    }
+
+    void SpawnLight()
+    {
+        // Instantiate the lightPrefab at the current position of the circle.
+        GameObject newLight = Instantiate(lightPrefab, transform.position, Quaternion.identity);
+    }
+}
