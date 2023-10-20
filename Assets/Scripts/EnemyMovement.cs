0833037fbb2e2ad0af9869f87c637b2fd319b8dc
Pedro <pedrosrvd@al.insper.edu.br>
2023-10-20 10:37:40 -0300

    animacoes

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,10 +8,12 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    Animator ani;
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        ani = this.GetComponent<Animator>();
         
     }
 
@@ -22,6 +24,7 @@ public class EnemyMovement : MonoBehaviour
         rb.rotation = angle;
         direction.Normalize();
         movement = direction;
+        ani.SetFloat("Angle", angle);
     }
 
     private void FixedUpdate()
